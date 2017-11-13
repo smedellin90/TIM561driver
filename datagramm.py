14dc594ab6a37aa299ae67ff33fcc2d1e3226242
Scott Medellin <scott.medellin90@gmail.com>
2017-11-12 23:11:51 -0800

    added datagramm.py

---
+++ datagramm.py
@@ -0,0 +1,55 @@
+# datagram.py 
+
+import collections
+
+tim561Datagram = collections.namedtuple("tim561Datagram",["CommandType", 
+															"Command", 
+															"VersionNumber",
+															"DeviceNumber", 
+															"SerialNumber", 
+															"DeviceStatus", 
+															"TelegramCounter", 
+															"ScanCounter",
+															"TimeSinceStartup", 
+															"TimeOfTransmission", 
+															"StatusOfDigitalInput",
+															"StatusOfDigitalOutput",
+															"LayerAngle",
+															"ScanFrequency",
+															"MeasurementFrequency",
+															"EncoderPosition",
+															"EncoderSpeed",
+															"OutputChannel16bit",
+															"ScaleFactor",
+															"ScaleFactorOffset",
+															"StartAngle",
+															"SizeofAngleStep",
+															"Data"
+															])
+
+
+def bytes_from_socket(socket):
+	while True:
+		data = socket.recv(256)
+		for byte in data:
+			yield bytes([data])
+
+def datagram_from_socket(socket):
+	STX = b'\x02'
+	ETX = b'\x03'
+
+	byte_generator = bytes_from_socket(socket)
+
+	while True:
+		datagram = b''
+
+		for byte in byte_generator:
+			if byte == STX: 
+				break
+
+		for byte in byte_generator:
+			if byte == ETX:
+				break
+			datagram += byte
+		yield datagram
+
